64ed78b56cfcff77c8d038fd4b2a0cd0ed85a980
Jiayu Liu <jimexist@gmail.com>
2015-03-26 00:18:04 -0400

    server list and hostandport

--- src/main/java/edu/cmu/raftj/runner/Runner.java
+++ src/main/java/edu/cmu/raftj/runner/Runner.java
@@ -1,5 +1,11 @@
 package edu.cmu.raftj.runner;
 
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.net.HostAndPort;
+import edu.cmu.raftj.server.DefaultServer;
+import edu.cmu.raftj.server.Server;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,11 +20,31 @@ public final class Runner {
     private Runner() {
     }
 
+    private static ImmutableList<HostAndPort> getServersList() {
+        return FluentIterable.of(System.getProperty("raftj.servers", "").split(","))
+                .transform(HostAndPort::fromString).toList();
+    }
+
+    private static HostAndPort getServerHostAndPort() {
+        return HostAndPort.fromString(System.getProperty("raftj.hostport", ""));
+    }
+
     public static void main(String[] args) {
         checkArgument(args.length == 1, "usage: <config_file_path>");
         String configFilePath = args[0];
         Path path = Paths.get(configFilePath);
         checkArgument(Files.exists(path) && Files.isRegularFile(path) && Files.isReadable(path),
                 "%s is not a regular readable file", path);
+
+        ImmutableList<HostAndPort> serversList = getServersList();
+        checkArgument(!serversList.isEmpty(), "server list cannot be empty");
+        checkArgument(serversList.size() % 2 == 1, "must be odd number");
+        HostAndPort hostAndPort = getServerHostAndPort();
+        checkArgument(serversList.contains(hostAndPort),
+                "host and port %s not in the given servers list %s", hostAndPort, serversList);
+
+        Server server = new DefaultServer(hostAndPort, serversList);
+
+
     }
 }
--- src/main/java/edu/cmu/raftj/server/DefaultServer.java
+++ src/main/java/edu/cmu/raftj/server/DefaultServer.java
@@ -1,5 +1,9 @@
 package edu.cmu.raftj.server;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.net.HostAndPort;
+
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -10,5 +14,12 @@ public class DefaultServer implements Server {
 
     private final AtomicLong currentTerm = new AtomicLong(0);
     private final AtomicReference<Role> currentRole = new AtomicReference<>(Role.Follower);
+    private final HostAndPort hostAndPort;
+    private final ImmutableList<HostAndPort> serversList;
+
+    public DefaultServer(HostAndPort hostAndPort, List<HostAndPort> serversList) {
+        this.hostAndPort = hostAndPort;
+        this.serversList = ImmutableList.copyOf(serversList);
+    }
 
 }
